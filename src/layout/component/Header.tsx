0dffc4b5a267ac9d257d7b978c9ae9d91df51d67
wuxue <wuxue@windimg.com>
2023-03-26 02:06:56 +0800

    product

--- src/layout/component/Header.tsx
+++ src/layout/component/Header.tsx
@@ -18,25 +18,6 @@ function Header(props: {
     <>
       <div>
         <div className="header flex items-center justify-between px-24 py-20 text-lg">
-          {/* <div className={'flex cursor-pointer items-center border-b-[4px] border-red pb-14'}>
-            <Image className="mr-2 h-20" src={'/src/assets/svg/chatgpt-logo.svg'} alt="chatgpt" />
-            {toPascal('ChatGPT')}
-          </div>
-          <div className="flex items-center">
-            <Image className="mr-2 h-20" src={'/src/assets/svg/bing-logo.svg'} alt="bing" />
-            {toPascal('bing')}
-          </div>
-          <div className="flex items-center">
-            <Image className="mr-2 h-20 " src={'/src/assets/svg/bard-logo.svg'} alt="bard" />
-            {toPascal('bard')}
-          </div> */}
-          {/* <Button className="bg-gradient" onClick={props.toggleTheme}> */}
-          {/* WeatherMoon24Filled */}
-          {/* WeatherMoon24Regular */}
-          {/* WeatherSunny24Filled */}
-          {/* WeatherSunny24Regular */}
-          {/* Toggle {props.isDarkMode ? 'light' : 'dark'}
-          </Button> */}
           <div>SSS</div>
           <div>{props.curProduct}</div>
           {props.isDarkMode ? (
