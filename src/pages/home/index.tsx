71ed2f90c5c3ba751acf3c4e5a57117c733f3839
wuxue <wuxue@windimg.com>
2023-03-25 23:02:55 +0800

    message

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import { Input, InputOnChangeData, Message } from '@fluentui/react-components'
+import { Input, InputOnChangeData } from '@fluentui/react-components'
 
 import { Search24Filled, Send24Regular } from '@fluentui/react-icons'
 import { useState } from 'react'
@@ -21,19 +21,27 @@ function Home() {
         content: message,
         from: 'me',
       },
+      {
+        content: '333',
+        from: 'other',
+      },
     ])
   }
   return (
     <>
       <div className="h-full py-24">
-        <div className="flex h-full flex-col  justify-between">
-          <div className="h-full w-full rounded-3xl bg-neutral-400  p-24">
+        <div className="flex h-full flex-col justify-between">
+          <div className="message-content h-full w-full rounded-3xl  p-24">
             <div className="mb-24 border-b-[1px] border-neutral-600 pb-24 text-center text-lg font-semibold">
               ChatGPT
             </div>
-            {messages.map((item, index) => (
-              <div key={index}>{item.content}</div>
-            ))}
+            <div className=" ">
+              {messages.map((item, index) => (
+                <div key={index} className={`mb-3  flex ${item.from === 'me' ? 'justify-end' : ''}`}>
+                  <div className="type-item select  inline-block rounded-lg py-1 px-2">{item.content}</div>
+                </div>
+              ))}
+            </div>
           </div>
           <div className="mt-24 flex items-center justify-between">
             <Search24Filled className="cursor-pointer" />
